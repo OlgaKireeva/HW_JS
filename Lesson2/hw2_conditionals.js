3dc15a7e4adf617fe1c8388f5620306e06199d83
Olga <kireevaolga451@gmail.com>
2023-11-07 10:55:08 +0200

    commit new branch

---
+++ Lesson2/hw2_conditionals.js
@@ -0,0 +1,9 @@
+var catsWeight = "str";
+if (catsWeight <= 5){
+console.log ("The cat is small and still needs to be fed");
+} else if (catsWeight > 5){
+    console.log ("The cat is very fluffy and needs to be combed")
+} else {
+    console.log ("It`s not a cat,silly:)")
+}
+
